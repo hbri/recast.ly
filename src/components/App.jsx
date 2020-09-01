622b9261c69ec14477cb24bc0cab720569616c22
ClaySamuelC <claysamuelc@gmail.com>
2020-08-31 17:28:00 -0700

    Correctly Render App, and VideoList

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,10 +7,10 @@ var App = () => (
     </nav>
     <div className="row">
       <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
+        <div><h5><em>videoPlayer</em>view goes here</h5></div>
       </div>
       <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+        <div><h5><em>videoList</em><div id="videoList"> view goes here </div></h5></div>
       </div>
     </div>
   </div>
@@ -19,3 +19,5 @@ var App = () => (
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 export default App;
+
+//<h5><em>videoList</em> view goes here</h5>
